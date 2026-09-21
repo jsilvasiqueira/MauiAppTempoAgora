6547bb81ac7f8f7005206e1bfd79c9d5fb57abe1
Juliana Siqueira <jsiqueira24@gmail.com>
2026-09-21 08:19:02 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
